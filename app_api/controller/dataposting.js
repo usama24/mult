f64c242ae4a6857d2cfc5ffe63ea0352c4cad387
Usama Amjad <usama2409@gmail.com>
2017-04-08 16:50:46 +0500

    changed

--- app_api/controller/dataposting.js
+++ app_api/controller/dataposting.js
@@ -11,15 +11,7 @@ var sendJSONresponse = function(res, status, content) {
        
 module.exports.uploadingData=function(req,res)
 {
-console.log("HELLO");
-  ursls.save(function(err){
-    if(err){
-      res.json(err);
-    }
-    else{
-      sendJSONresponse(res,200,{
-        "Message":req.file.url
-      })
-    }
-  })
+sendJSONresponse(res,200,{
+  "Message":req.file.url;
+})
 }
